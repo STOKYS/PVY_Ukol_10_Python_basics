dd4c1ec6b71c47c9aa9a34f7915273cc3f7a64f6
STOKYS <dejv2004s@gmail.com>
2021-09-25 15:37:28 +0200

    Added 02-fundamentals

--- 02-fundamentals/physics.py
+++ 02-fundamentals/physics.py
@@ -1,22 +1,49 @@
 '''
-Konstanty v Pythonu
-
-Konstanta je vlastně speciální typ proměnné, jejíž hodnota nemůže být změněna.
-V Pythonu jsou konstanty obvykle deklarovány a přiřazovány v modulu, který bývá importován do souboru aplikace.
-Konstanty jsou pojmenovány velkými písmeny a jednotlivá slova jsou oddělována podtržítky.
-'''
-
-EARTH_GRAVITY = 0 #? normální pozemské tíhové zrychlení
-MOON_GRAVITY = 0 #? měsíční gravitace
-SPEED_OF_LIGHT = 0 #? rychlost světla ve vakuu
-SPEED_OF_SOUND = 0 #? rychlost zvuku při teplotě 20 °C v suchém vzduchu
-
-''' 
 Úkol:
 1. Doplňte správně hodnoty uvedených konstant.
 2. Doplňte physics.py o několik výpočtových funkcí (opatřené docstrings), v nichž využijete minimálně všechny výše uvedené konstanty.
 Samozřejmě můžete své řešení rozšířit i o jiné fyzikální konstanty.
-3. Vytvořte z tohoto souboru samostatný modul v Pythonu podle návodu, který si sami najdete na internetu.      
+3. Vytvořte z tohoto souboru samostatný modul v Pythonu podle návodu, který si sami najdete na internetu.
 4. Vytvořte vlastní aplikaci myapp.py, do níž tento modul importujte. Demonstrujte v ní na jednoduchých příkladech využití vámi
-připravených funkcí.  
+připravených funkcí.
 '''
+
+EARTH_GRAVITY = 9.807 #? normální pozemské tíhové zrychlení
+MOON_GRAVITY = 1.62 #? měsíční gravitace
+SPEED_OF_LIGHT = 299792458 #? rychlost světla ve vakuu
+SPEED_OF_SOUND = 343 #? rychlost zvuku při teplotě 20 °C v suchém vzduchu
+
+
+def fnc_weight(weight):
+    """
+    Calculates how much would you weight on the moon
+    """
+    calc = float(weight) / EARTH_GRAVITY * MOON_GRAVITY
+    print(f"Your weight on the moon would be: {round(calc, 3)}kg\n")
+
+
+def fnc_how_far(time):
+    """
+    Calculates how far away from you is the source of sound,
+    can be useful for finding out how far a thunder is from you
+    """
+    calc = SPEED_OF_SOUND * float(time)
+    print(f"The source of the sound is about {round(calc, 3)}m away from you\n")
+
+
+def fnc_how_long(distance):
+    """
+    Calculates how long would it take for light to travel there
+    """
+    calc = (float(distance) / SPEED_OF_LIGHT)
+    print(f"It would take {round(calc, 10)} seconds for light to travel there\n")
+
+
+def main():
+    fnc_weight(input("Input your weight (kg):  "))
+    fnc_how_far(input("How long did it take for you to hear the sound (s):  "))
+    fnc_how_long(input("How far would you like to travel (m):  "))
+
+
+if __name__ == "__main__":
+    main()
--- 02-fundamentals/strings.py
+++ 02-fundamentals/strings.py
@@ -1,149 +1,4 @@
-# Znakové řetězce mohou být uvozeny jednoduchými i dvojitými uvozovkami
-course_name = "Python programming"
-
 '''
-Mnohdy programátor potřebuje řetězec, který je rozložen přes více řádků. Dosáhnout toho opět můžeme několika způsoby. 
-První z nich, který se neváže pouze na řetězce, je spojení dvou po sobě jdoucích řádků znakem zpětného lomítka:
-'''
-# Obrácené lomítka - použití escape sekvence
-hello = 'Toto je dlouhý řetězec obsahující mnoho\n\
-řádek textu, stejně jej zapisujete i v C.\n\
-    "Bílé" znaky na začátku řádku se samozřejmě\
- berou v úvahu.'
-
-# print(hello)
-
-'''
-Python podporuje i tzv. raw řetězce (cosi jak ryzí, syrové řetězce), u nichž se řídící (escape) sekvence 
-nepřevádí na odpovídající znaky.3.2. Raw řetězce charakterizuje předpona r. 
-Potom řetězec r'\n' odpovídá dvěma znakům - zpětnému lomítku a znaku n, kdežto řetězec '\n' je jediný znak nového řádku:
-'''
-
-hello = r'Toto je dlouhý řetězec obsahující mnoho\n\
-řádek textu, stejně jej zapisujete i v C.'
-
-# print(hello)
-
-'''
-Další možností, jak vytvořit víceřádkový řetězec je jeho uzavření mezi odpovídající pár trojitých uvozovek. 
-To má tu výhodu, že nemusíme explicitně zapisovat konce řádků, ty bude řetězec obsahovat přesně tak, 
-jak jsou zapsány ve zdrojovém kódu
-'''
-# Předformátovaný text je uvozen trojími uvozovkami
-message = '''
-    Krásný pozdrav z výletu do \"přírody\"
-
-    posílá 
-
-       Andrej
-'''
-# print(message)
-
-# Funkce len vrací délku řetězce
-# print(len(message))
-
-'''
-Řetězce můžeme (podobně jako v jazyce C) indexovat. První znak řetězce pak má index 0. 
-Poněvadž je Python velice uživatelsky přítulný, nekomplikuje život programátora speciálním typem určeným pro jediný znak - 
-každý znak řetězce je opět řetězec s délkou 1. 
-Na získání podřetězce nepotřebujeme žádné speciální funkce, samotný jazyk podporuje indexování subsekvencí. 
-Subsekvenci indexujeme podobně jako jednotlivé znaky, pouze potřebuje dva indexy (začátek a konec subsekvence), 
-které oddělíme dvojtečkou:
-'''
-
-# Vypíše 4. znak z řetězce
-# print(course_name[3])
-
-'''
-Důležité je si zapamatovat, že slice indexy ukazují mezi znaky, přičemž levá hrana prvního znaku má číslo 0 
-a pravá hrana posledního znaku řetězce o n znacích má index n:
-
- +---+---+---+---+---+ 
- | H | e | l | p | A |
- +---+---+---+---+---+ 
- 0   1   2   3   4   5 
--5  -4  -3  -2  -1
-Na prvním řádku jsou uvedeny všechny možné slice-indexy 0...5 v řetězci 'HelpA', na druhém pak odpovídající záporné hodnoty. 
-Řez od i do j je tedy tvořen všemi znaky mezi hranami označenými mezi hranami označenými i a j.
-'''
-
-# Vypíše poslední znak z řetězce
-# print(course_name[-1])
-# Vypíše vše od 2. do 4. znaku (bez něj)
-# print(course_name[1:3])
-
-'''
-Slice indexy mají specifické vlastnosti. Vynecháme-li první index, je za něj automaticky dosazena nula (začátek řetězce). 
-Při neuvedení druhého indexu se použije délka řetězce (čili konec řetězce). 
-'''
-
-# Vypíše vše od 2. znaku
-# print(course_name[1:])
-# Vypíše vše od 2. znaku do předposledního
-# print(course_name[1:-1])
-# Vypíše 3 znaky od začátku
-# print(course_name[:3])
-
-'''
-Další vlastností slice indexů je jejich automatické "zarovnávání" na rozměr řetězce. 
-Je-li totiž index použitý ve slice konstrukci příliš velký, je nahrazen délkou řetězce. 
-'''
-# print(course_name[1:50])
-# Pokud je dolní index větší než horní, je výsledkem prázdný řetězec:
-# print(course_name[2:1])
-
-first_name = "Donald  "
-second_name = "  Trump"
-# Spojování řetězců
-# print(first_name + second_name)
-
-# Opakování řetězců
-# print(first_name + second_name * 3)
-
-'''
-Řetězce v jazyce Python nelze měnit. Pokusíme-li se změnit určitou pozici v řetězci, dojde k chybě.
-'''
-# first_name[0] = 'R'
-'''
-Proto jedinou cestou, jak vytvářet nové řetězce, je jejich kombinování, které je velice jednoduché a přitom efektivní:
-'''
-# print('R' + first_name[1:])
-
-# Formátovaný výstup, použití řetězcových funkcí
-# print(f"{first_name.upper().rstrip()} {second_name.lower().lstrip()} {5 * 4} {len(message)}")
-
-# Nalezení pozice podřetězce v řetězci
-# print(first_name.find("na"))
-
-# Nahrazení podřetězce v řetězci
-# print(first_name.upper().replace("D", "*"))
-
-# Zjištění výskytu podřetězce
-# print("na" not in first_name)
-
-'''
-Python umí pracovat s Unicode řetězci úplně stejným způsobem jako s obyčejnými řetězci. 
-Dokonce je možné díky konverzním funkcím snadno převádět obyčejné řetězce na Unicode a zpět.
-Unicode řetězce můžeme zapisovat přímo ve zdrojovém kódu programu. 
-Pouze před samotný řetězec vložíme prefix u (podobně jako u raw řetězců prefix r):
-'''
-
-# print(u'\xe4\xf6\xfc')
-
-'''Pro konverzi znaků můžeme použít interní funkci encode(), 
-která umožňuje přístup ke všem registrovaným kodekům (např. Latin-1, ASCII, UTF-8 nebo UTF-16). '''
-
-# print(u'äöü'.encode('utf-8'))
-# print(u'čřž'.encode('latin2'))
-
-'''Opačnou konverzi umožňuje funkce decode(), které lze opět předat jediný argument - jméno kódování, 
-ve kterém je původní osmibitový řetězec.
-'''
-
-# print(b'\xc3\xa4\xc3\xb6\xc3\xbc'.decode('utf-8'))
-# print(b'\xe8\xf8\xbe'.decode('windows-1250'))
-
-''' 
 Programátorská výzva:
 Použijte kombinaci různý možností pro práci s řetězci (včetně různých funkcí) i jiných prvků jazyka Python 
 (ternární operátor, matematické funkce atd.) k co nejefektivnějšímu řešení následujících cvičení (čím kratší
@@ -158,3 +13,41 @@ funkční kód, tím lepší).
 3. Vytvořte funkci, která vygeneruje náhodná hesla pro počet osob zadaný v parametru tak, aby heslo začínalo
    3 velkými písmeny, pokračovalo 3 malými písmeny, jedním speciálním znakem (-/+*) a končilo 3 náhodnými číslicemi.
 '''
+
+
+def fnc_date_converter():
+    date = (input("Type in a valid date(dd.mm.yyyy):  ")).split(".")
+    print(f"{date[2]}-{date[1]}-{date[0]}")
+
+
+def fnc_snake_case():
+    vstup = (input("Type in a short sentence:  ")).split(" ")
+    fin = ''
+    for i in range(0, len(vstup)):
+        fin += (f"{vstup[i].lower()}")
+        if (i + 1) != len(vstup):
+            fin += ("_")
+    print(fin)
+
+
+def fnc_camel_case():
+    vstup = (input("Type in a short sentence:  ")).split(" ")
+    fin = ''
+    for i in range(0, len(vstup)):
+        if not i:
+            fin += ((f"{vstup[i].lower()}"))
+        else:
+            fin += (f"{vstup[i][0].upper()}{vstup[i][1:].lower()}")
+    print(fin)
+
+
+def main():
+    fnc_date_converter()
+    fnc_snake_case()
+    fnc_camel_case()
+
+
+if __name__ == "__main__":
+    main()
+
+
